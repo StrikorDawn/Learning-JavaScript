93a5c511ef90277d8b22e27aba239467499341e8
StrikorDawnh <tadawg007@byui.edu>
2023-11-10 23:33:35 -0700

    Finished the DOM Basics activity

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,29 +1,27 @@
 /* W02-Task - Profile Home Page */
 
 /* Step 1 - Setup type tasks - no code required */
-
+// Done!
 /* Step 2 - Variables */
-
-
-
+let fullName = "Taden Marston";
+let currentYear = "2023";
+let profilePicture = "images/IMG_0067.JPG";
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.getElementById('#year');
+// const pictureElement = document.get("home");
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContext = `${currentYear}`;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute("alt", fullName)
 
 /* Step 5 - Array */
-
-
-
-
+let favoriteFoods = ["Pizza", "Cheeseburger", "Sushi"];
+let mostFavoriteFood = favoriteFoods[3];
+foodElement document.
 
 
