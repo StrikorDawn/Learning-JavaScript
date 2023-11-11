319faffc896459ee37ef1af646025c12fd74ad6f
StrikorDawnh <tadawg007@byui.edu>
2023-11-11 11:15:31 -0700

    used support to do step 5, researched why it works

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -10,8 +10,8 @@ let profilePicture = "images/IMG_0067.JPG";
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
-const yearElement = document.getElementById('#year');
-// const pictureElement = document.get("home");
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
@@ -20,8 +20,13 @@ imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute("alt", fullName)
 
 /* Step 5 - Array */
-let favoriteFoods = ["Pizza", "Cheeseburger", "Sushi"];
-let mostFavoriteFood = favoriteFoods[3];
-foodElement document.
-
+const favoriteFoods = ['Sushi', 'Cheeseburger', 'Pizza','Taco Soup'];
+foodElement.innerHTML = `<ul>${favoriteFoods.map(food => `<li>${food}</li>`).join('')}</ul>`;
+const newFavoriteFood = 'Orange Chicken';
+favoriteFoods.push(newFavoriteFood);
+foodElement.innerHTML += `<br>${favoriteFoods.join(', ')}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods.join(', ')}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods.join(', ')}`;
 
